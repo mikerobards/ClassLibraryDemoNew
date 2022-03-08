ab2d0e1e31b70a00f1f50406fcdd1106b387a322
Michael Robards <mikerobards@icloud.com>
2022-03-08 17:07:52 -0500

    finish lesson and homework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿using DemoLibrary.Models;
 
+PersonModel person = new PersonModel();
 
+Console.Write("What is your first name: " );
+person.FirstName = Console.ReadLine();
+Console.Write("What is your last name: ");
+person.LastName = Console.ReadLine();
 
+Console.WriteLine($"Hello { person.FirstName } {person.LastName}");
